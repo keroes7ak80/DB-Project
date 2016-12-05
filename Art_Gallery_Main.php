0773d40a3dd552a62e8717937cdd3a6e2b98027e
kero_es7ak <kero_es7ak@KERO2>
2016-12-05 18:46:34 +0200

    first commit

---
+++ Art_Gallery_Main.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Art Gallery</title>
+        <link rel='stylesheet'href='css/bootstrap.css'>
+        <link rel='stylesheet'href='css/style.css'>
+        <link href="https://fonts.googleapis.com/css?family=Lobster|Pacifico" rel="stylesheet">
+
+    </head>
+    <body>
+  <div class="container-fluid">
+  <nav class="navbar navbar-inverse ">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+          <li><a href="http://www.w3schools.com/default.asp">Artists</a> </li>
+           <li><a href="http://www.w3schools.com/default.asp">Artworks</a> </li>
+          </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <!--<button type="button" class="btn btn-default navbar-btn">Log out</button>-->
+          
+        <button type="button" class="btn btn-default btn-md" data-toggle="modal" data-target="#myModal">Log out</button>
+      </ul>
+    </div>
+    
+    
+   </body>
+  </html>
+
